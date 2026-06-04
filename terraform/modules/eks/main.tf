7b5d7a9b44f3c6e5220b33b3b5ebe882fe96c240
anuragstark <anuragchauhan536@gmail.com>
2026-06-04 12:25:08 +0530

    fix: scale to 2 nodes t3.small

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -119,8 +119,8 @@ resource "aws_eks_node_group" "this" {
   subnet_ids      = var.subnet_ids
 
   scaling_config {
-    desired_size = 1
-    max_size     = 2
+    desired_size = 2
+    max_size     = 3
     min_size     = 1
   }
 
