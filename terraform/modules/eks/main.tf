95bc0a35982a3c46ad2a09b92357a4e41d33dd51
anuragstark <anuragchauhan536@gmail.com>
2026-06-04 10:39:01 +0530

    fix: allow all VPC traffic in security group for node registration

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -4,11 +4,20 @@ resource "aws_security_group" "eks_sg" {
   description = "Security group for EKS cluster"
   vpc_id      = var.vpc_id
 
+  # Allow HTTPS from anywhere (for kubectl and API access)
   ingress {
     from_port   = 443
     to_port     = 443
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Allow all traffic within VPC (node-to-control-plane communication)
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["10.0.0.0/16"]
   }
 
   egress {
