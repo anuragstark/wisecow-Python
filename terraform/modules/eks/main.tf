0c3ea5271f11361dc8426b2dd5842ff2f88f1a34
anuragstark <anuragchauhan536@gmail.com>
2026-06-04 11:29:17 +0530

    fix: use t3.small (2GB, Free Tier eligible)

--- terraform/modules/eks/main.tf
+++ terraform/modules/eks/main.tf
@@ -125,7 +125,7 @@ resource "aws_eks_node_group" "this" {
   }
 
   capacity_type  = "ON_DEMAND"
-  instance_types = ["t3.medium"]
+  instance_types = ["t3.small"]
 
   depends_on = [
     aws_iam_role_policy_attachment.worker_node_policy,
